2fd87a3ba270e99d549c7eba7be1b47b12601cdd
Ymar Frenken <ymarfrenken@gmail.com>
2024-10-20 12:31:12 +0200

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
-
+3
 
 export default defineConfig({
   site: "https://ymar.nl",
   integrations: [mdx(), sitemap(), tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
